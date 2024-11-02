e021d0ff3a9e6a1019b0587b06c2f6fcb1038e3b
Malthe Poulsen <malthe@grundtvigsvej.dk>
2024-11-02 10:19:49 +0100

    Y2022D01P1

---
+++ Y2022/Day01.cs
@@ -0,0 +1,25 @@
+﻿namespace AdventOfCode.Y2022;
+
+public class Day01 : Day
+{
+  public override (int part1, int? part2) Solve(string[] input)
+  {
+    var weightTotals = new List<int>();
+    var currentWeight = 0;
+    foreach (var s in input)
+    {
+      if (int.TryParse(s, out var number))
+      {
+        currentWeight += number;
+      }
+      else if (currentWeight > 0)
+      {
+        weightTotals.Add(currentWeight);
+        currentWeight = 0;
+      }
+    }
+
+    weightTotals.Sort((a, b) => b.CompareTo(a));
+    return (weightTotals[0], null);
+  }
+}
