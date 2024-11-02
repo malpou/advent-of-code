59ca94f619a6f16174989151a4b3f23850877684
Malthe Poulsen <malthe@grundtvigsvej.dk>
2024-11-02 13:55:54 +0100

    fmt

--- Y2022/Day03.cs
+++ Y2022/Day03.cs
@@ -4,18 +4,17 @@ public class Day03 : Day
 {
   public override (int part1, int? part2) Solve(string[] input, string _)
   {
-    var part1 = input.Select(s =>
-        s[..(s.Length / 2)]
-          .Intersect(s[(s.Length / 2)..])
-          .Sum(GetPriority))
+    var part1 = input.Select(s => s[..(s.Length / 2)]
+        .Intersect(s[(s.Length / 2)..])
+        .Sum(GetPriority))
       .Sum();
 
     var part2 = input
       .Chunk(3)
       .Select(group => group[0]
-          .Intersect(group[1])
-          .Intersect(group[2])
-          .Sum(GetPriority))
+        .Intersect(group[1])
+        .Intersect(group[2])
+        .Sum(GetPriority))
       .Sum();
 
     return (part1, part2);
